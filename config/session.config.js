07ecc630d0bd86211fd84985e7303375d8870d78
Philipp Kastl <picastle@posteo.de>
2022-02-18 10:41:02 +0100

    fixed session config

--- config/session.config.js
+++ config/session.config.js
@@ -3,19 +3,22 @@ const MongoStore = require('connect-mongo');
 const { MONGODB_URL, NODE_ENV, SESSION_SECRET } = process.env;
 
 function sessionConfig(app) {
+  const isProduction = NODE_ENV === 'production';
+  const sameSite = isProduction ? 'none' : 'lax';
+
   app.set('trust proxy', 1);
   app.use(
     session({
       secret: SESSION_SECRET,
-      resave: false,
-      saveUninitialized: true,
+      resave: true,
+      saveUninitialized: false,
       store: MongoStore.create({
         mongoUrl: MONGODB_URL,
       }),
       cookie: {
         maxAge: 1000 * 24 * 60 * 60 * 365,
-        sameSite: false,
-        secure: NODE_ENV === 'production',
+        sameSite: sameSite,
+        secure: isProduction,
       },
     })
   );
